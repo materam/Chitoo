89d65cb55e46f1cf0582fb29ba5a552a81075b19
Anmol <anmolpradhan966@gmail.com>
2024-11-27 21:46:26 +0530

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>
+            ""Favorite खाना order गर्नुस्"</h2>
+            <p>"Welcome to Chitoo, your ultimate destination for discovering and ordering mouthwatering dishes!  Hungry? Start your flavorful journey now!"</p>
+            {/* <button>View Menu</button> */}
+        </div>
+    </div>
+  )
+}
+
+export default Header
