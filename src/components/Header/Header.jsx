10c6e956ec3389735b36210ddbcfe630e7576c6e
Anmol <anmolpradhan966@gmail.com>
2024-11-27 21:57:40 +0530

    refactor: spelling of nepali word.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>
-            ""Favorite खाना order गर्नुस्"</h2>
+            "Favorite खाना order गर्नुहोस्"</h2>
             <p>"Welcome to Chitoo, your ultimate destination for discovering and ordering mouthwatering dishes!  Hungry? Start your flavorful journey now!"</p>
             {/* <button>View Menu</button> */}
         </div>
